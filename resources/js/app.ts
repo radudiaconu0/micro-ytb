6d699e0b0d803d7a4d4b70a8e408f864f2a13619
Diaconu Radu-Mihai <52667211+radudiaconu0@users.noreply.github.com>
2024-09-07 18:12:17 +0300

    add apollo and vue apollo

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,10 +2,27 @@ import './bootstrap';
 import '../css/app.css';
 import 'flowbite';
 
-import { createApp, h, DefineComponent } from 'vue';
+import { createApp, h, DefineComponent, provide } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+    // You should use an absolute URL here
+    uri: 'https://micro-ytb.test/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+})
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,6 +30,7 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
+        provide(DefaultApolloClient, apolloClient)
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
